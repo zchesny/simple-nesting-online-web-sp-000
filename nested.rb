7eefb6705c7056fbf4562b89d0c2cc9499b67229
Zoey Chesny <z.chesny@gmail.com>
2020-04-13 18:37:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,6 +108,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
+		 programmer_hash[:alans_kay][:known_for] = alans_new_info
+		 programmer_hash
 
 
 end
@@ -128,5 +130,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+	programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+	programmer_hash
 
 end
